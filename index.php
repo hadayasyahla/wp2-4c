8deba537ca4453f800afe8378ac8b8424745539f
hadayasyahla <hadayasyahla@gmail.com>
2022-03-16 17:37:54 +0700

    first

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<h3>selamat datang</h3>
+<?php
+include('koneksi.php');
+
+$sql = "select * from mahasiswa";
+$result = pg_query($sql);
+
+?>
+<h1>Aplikasi Mahasiswa</h1>
+
+<table border="1">
+    <thead>
+        <tr>
+            <tb>NIM</tb>
+            <tb>NAMA</tb>
+            <tb>KELAS</tb>
+        </tr>
+    </thead>
+<tbody>
+
+<?php
+    while($row = pg_fetch_object($result));
+?>
