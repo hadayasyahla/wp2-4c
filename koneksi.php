2466be6814b1458aee7f5e390fa830bdd7d7f436
hadayasyahla <hadayasyahla@gmail.com>
2022-03-16 17:13:53 +0700

    two

--- koneksi.php
+++ koneksi.php
@@ -3,12 +3,12 @@ $conn_str = "host=kashin.db.elephantsql.com " .
 "port=5432 " . 
 "user= 	gpelewwe " . 
 "dbname= 	gpelewwe " . 
-"password=gBzORR0XfYn6-PQujfee4GchfiiO-XTg "
+"password=gBzORR0XfYn6-PQujfee4GchfiiO-XTg ";
 $conn = pg_connect($conn_str);
 
-if($conn)(
+if($conn){
     echo"<h3>koneksi berhasil</h3>";
-)else{
+}else{
     echo"<h3>koneksi GAGAl TERSAMBUNG</h3>";
 }
 ?>
